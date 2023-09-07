faf593d36f588a40fc843cf5800446d14fa8cdc3
wky0710 <89335841+wky0710@users.noreply.github.com>
2023-09-07 15:32:18 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.culwxou6cuaj.us-east-1.rds.amazonaws.com"
+customhost = "database-assignment.culwxou6cuaj.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
-custombucket = "wongkaryan-employee"
+customdb = "student"
+custombucket = "wongkaryan-student"
 customregion = "us-east-1"
 
